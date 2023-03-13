8802c0885e47465fafcd4de986a1a64ba455aa1b
Beyza Çabuk <beyzancabuk@gmail.com>
2023-03-13 18:55:24 +0300

    Add files via upload

---
+++ PostgreSQL-patika.hw7.sql
@@ -0,0 +1,19 @@
+--Q1 
+SELECT rating, COUNT (*) FROM film
+GROUP BY rating;
+
+--Q2
+SELECT replacement_cost, COUNT (*) FROM film
+GROUP BY replacement_cost
+HAVING COUNT (*) > 50
+ORDER BY replacement_cost;
+
+--Q3
+SELECT store_id, COUNT (*) FROM customer
+GROUP BY store_id;
+
+--Q4
+SELECT country_id, COUNT (*) FROM city
+GROUP BY country_id
+ORDER BY COUNT (*) DESC
+LIMIT 1;
